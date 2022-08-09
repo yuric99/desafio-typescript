11313ecb0d1eb2eb921f9cdb6105c8110bd4705e
yuric99 <yuri.c.donascimento@gmail.com>
2022-08-09 12:59:28 -0300

    Desafios

---
+++ desafios/desafio1.ts
@@ -0,0 +1,9 @@
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+let employee = {
+    code: 10,
+    name: 'Jhon'
+};
+
+
+
---
+++ desafios/desafio2.ts
@@ -0,0 +1,27 @@
+// Como podemos melhorar o esse código usando TS? 
+
+let pessoa1 = {
+    nome: "maria",
+    idade: 29,
+    profissao: "atriz"
+};
+
+
+let pessoa2 = {
+    nome: "roberto",
+    idade: 19,
+    profissao: "Padeiro"
+}
+
+
+let pessoa3 = {
+    nome: "laura",
+    idade: "32",
+    profissao: "Atriz"
+};
+
+let pessoa4 = {
+    nome: "carlos",
+    idade: 19,
+    profissao: "padeiro"
+}
